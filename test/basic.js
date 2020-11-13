f7dc3d3e3968ca8b4ec57bf85e889c198d556e1b
Matt Simerson <matt@tnpi.net>
2020-11-13 13:08:39 -0800

    replace travis/appveyor with github actions (#48)
    * replace travis/appveyor with github actions
    * test: when splitting lines, use os.EOL

--- test/basic.js
+++ test/basic.js
@@ -6,12 +6,13 @@ const path   = require('path')
 const parse  = require('../index').parse;
 
 const raw_data = fs.readFileSync(path.join(__dirname, 'emails.txt'), "UTF-8");
+const EOLRE  = new RegExp(require('os').EOL)
 
 const tests = raw_data.split(/\n\n/).map(function (rows) {
-    const lines = rows.split(/\n/);
+    const lines = rows.split(EOLRE);
     // console.log(lines)
     if (lines[0] === '') lines.shift();
-    return lines.filter(function (l) { return !/^#/.test(l) });
+    return lines.filter((l) => { return !/^#/.test(l) });
 });
 
 describe('basic parse', function () {
@@ -25,10 +26,10 @@ describe('basic parse', function () {
             if (test[2]) details.name = test[2];
 
             const parsed = parse(test[0])[0];
-            // console.log('Parsed', parsed);
+            // console.log(`Parsed: ${parsed}`);
 
             for (const k in details) {
-                assert.equal(parsed[k](), details[k], `Test '${k}' for '${parsed[k]()}' = '${details[k]}' from ${JSON.stringify(parsed)}`);
+                assert.strictEqual(parsed[k](), details[k], `Test '${k}' for '${parsed[k]()}' = '${details[k]}' from ${JSON.stringify(parsed)}`);
             }
         })
     })
